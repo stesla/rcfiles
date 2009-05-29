76bd51580ed1da4bfe12ef00ff008f73cced9d22
Samuel Tesla <stesla@engineyard.com>
2009-05-29 15:39:06 -0700

    Remove emacs config

--- home/.elisp/slime/bootstrap.lisp
+++
@@ -1,5 +0,0 @@
-;; Load Swank
-(load (merge-pathnames ".elisp/slime/swank-loader" (user-homedir-pathname)))
-
-;; Save image
-(sb-ext:save-lisp-and-die "sbcl-with-slime.core")
--- home/.elisp/slime/swank-loader.lisp
+++
@@ -1,4 +0,0 @@
-(if (not (find-package 'swank-loader))
-    ;; Edit SLIME-DIR to be where you have SLIME installed.
-    (let ((slime-dir (merge-pathnames ".elisp/slime/" (user-homedir-pathname))))
-      (load (merge-pathnames "swank-loader-original" slime-dir))))
