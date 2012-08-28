3ebddc007ca45b006afc2e6fd0f7a8e7fa312f17
Samuel Tesla <stesla@drw.com>
2012-08-28 12:10:15 -0500

    customize tabwidth

--- init.el
+++ init.el
@@ -7,3 +7,15 @@
 
 (color-theme-zenburn)
 (server-start)
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(tab-width 4))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
