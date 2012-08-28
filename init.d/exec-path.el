5aa457ed14cff83556e8982a6e77fe45e3464f71
Samuel Tesla <stesla@drw.com>
2012-08-28 12:09:43 -0500

    get LD_LIBRARY_PATH from bash

--- init.d/exec-path.el
+++ init.d/exec-path.el
@@ -2,3 +2,7 @@
 (let ((path (shell-command-to-string "/bin/bash --login -c 'echo -n \"$PATH\"'")))
   (setenv "PATH" path)
   (setq exec-path (split-string path ":")))
+
+
+(let ((path (shell-command-to-string "/bin/bash --login -c 'echo -n \"$LD_LIBRARY_PATH\"'")))
+  (setenv "LD_LIBRARY_PATH" path))
