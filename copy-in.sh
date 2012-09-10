d02451c44877bf5923dddd6253c38a67768bf976
Samuel Tesla <stesla@drw.com>
2012-09-10 15:13:57 -0500

    reorganize how scripts are managed

--- copy-in.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-cd ${0%/*}
-
-find home -type f -print | while read file; do
-    cp -v $(sed -e "s#home#$HOME#" <<< "$file") $file
-done
--- copy-out.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-cd ${0%/*}
-
-find home -type f -print | while read file; do
-    dest=$(sed -e "s#home#$HOME#" <<< "$file")
-    mv -v $dest $dest.bak
-    cp -v $file $dest
-done
