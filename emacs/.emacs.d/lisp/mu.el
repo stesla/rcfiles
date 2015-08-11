7b5441d487cb7503a78806dc2668f6ef4389f1bc
Samuel Tesla <stesla@drw.com>
2015-08-11 16:26:57 -0500

    add more delete-indentation functions

--- emacs/.emacs.d/lisp/mu.el
+++ emacs/.emacs.d/lisp/mu.el
@@ -1,7 +1,17 @@
-(defun delete-indentation-in-region (start end)
-  "Remove all the indentation in the region by repeatedly calling DELETE-INDENTATION"
+(defun delete-indentation-region (start end)
+  "Remove all the indentation in the region"
   (interactive "*r")
   (save-excursion
     (goto-char end)
     (while (> (point) start)
       (delete-indentation))))
+
+(defun delete-indentation-paragraph (arg)
+  "Remove all indentation in the paragraph at POINT"
+  (interactive "*p")
+  (save-excursion
+    (let ((end   (progn (forward-paragraph arg) (point)))
+          (start (progn (backward-paragraph arg) (point))))
+      (delete-indentation-region start end))))
+
+(provide 'mu)
